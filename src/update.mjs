694cad6c2f867660b48d954174521ae207676d5d
jihchi <achi@987.tw>
2022-01-09 11:33:59 +0200

    Fix incorrect wrapping in script tag

--- src/update.mjs
+++ src/update.mjs
@@ -29,7 +29,7 @@ $$('meta[property="og:url"]').attr('content', 'https://infofinland.tw');
 $$('meta[property="og:image"]').attr('content', 'https://infofinland.tw/assets/twitter_header_photo_1_1641713421419_0.png');
 $$('meta[property="og:description"]').attr('content', '提供🇫🇮芬蘭相關資訊');
 $$('meta[property="og:site_name"]').attr('content', 'InfoFinland 中文台灣');
-$$('head').append('script').html(googleAnalytics);
+$$('head').append(googleAnalytics);
 
 await fs.outputFile(indexHtml, $$.html());
 
